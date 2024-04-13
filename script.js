b427388b2b1e6d997ccb57bcabe34b0f8bfea1f6
shin <121587677+shinkoushi@users.noreply.github.com>
2024-04-13 13:59:51 +0700

    script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const inputTime = document.getElementById("inputTime");
 const inputDay = document.getElementById("day");
 const listContainer = document.getElementById("list-container");
 const info = document.getElementById("info")
+const countElement = document.getElementById("keterangan"); // added this line to select the countElement
 
 
 function countWords(str) {
@@ -15,7 +16,7 @@ function AddTask() {
       icon: "error",
       text: "kamu belum menulis pekerjaan mu !",
   });
-  
+
   }else if(inputTime.value == ""){
     Swal.fire({
       icon: "error",
@@ -37,31 +38,33 @@ function AddTask() {
     span.innerHTML = "\u00d7";
     li.appendChild(span);
 
-
     let icon = document.createElement("i");
-    icon.classList.add('icon'); 
+    icon.classList.add('icon');
     icon.innerHTML = `<i class="fa-solid fa-calendar-days"></i>`;
-    
+
     if (countWords(li.innerHTML) > 8 && window.innerWidth <= 600) {
-      icon.style.top = "10px";
-     
-    } else if (countWords(li.innerHTML) > 4 && window.innerWidth <= 400) {
+      icon.style.top = "8px";
+
+    }else if (countWords(li.innerHTML) > 6 && window.innerWidth <= 500) {
+      icon.style.top = "8px";}
+
+     else if (countWords(li.innerHTML) > 4 && window.innerWidth <= 400) {
       icon.style.top = "8px";
     }
 
     span.appendChild(icon);
 
-
     let dayElement = document.createElement("p");
-    dayElement.classList.add('day'); 
+    dayElement.classList.add('day');
     dayElement.innerHTML = inputDay.value;
     icon.appendChild(dayElement);
-    
+
     let timeElement = document.createElement("hp");
-    timeElement.classList.add('time'); 
+    timeElement.classList.add('time');
     timeElement.innerHTML = inputTime.value;
     dayElement.appendChild(timeElement)
- 
+
+    showUncheckedLiCount(); // added this line to update the countElement after adding a new task
   }
   inputBox.value = "";
   inputTime.value = "";
@@ -69,7 +72,7 @@ function AddTask() {
 
   info.style.display = "none"
   saveData();
-  
+
 }
 
 listContainer.addEventListener(
@@ -90,10 +93,28 @@ listContainer.addEventListener(
 function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
 }
+
 function showTask() {
   listContainer.innerHTML = localStorage.getItem("data");
   if (listContainer.getElementsByTagName("li").length > 0) {
     info.style.display = "none";
   }
+  showUncheckedLiCount();
 }
 showTask();
+
+function countUncheckedLi() {
+  const liElements = listContainer.getElementsByTagName("li");
+  let count = 0;
+  for (let i = 0; i < liElements.length; i++) {
+    if (!liElements[i].classList.contains("checked")) {
+      count++;
+    }
+  }
+  return count;
+}
+
+function showUncheckedLiCount() {
+  const uncheckedCount = countUncheckedLi();
+  countElement.textContent = `Kamu memiliki ${uncheckedCount} tugas yang belum diselesaikan`;
+}
